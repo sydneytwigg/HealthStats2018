adb92d7b0dbf5f278d8ed429f8186b4773d7ac32
Sydney <sydneytwigg@gmail.com>
2019-01-02 18:35:15 +0000

    Added column chart for hours per month over the year

---
+++ data/histogram_data.js
@@ -0,0 +1,15 @@
+var data = google.visualization.arrayToDataTable([
+  ['Month', 'Total Duration'],
+  ['January', 11.71],
+  ['February',	8.36],
+  ['March',	11.36],
+  ['April', 0.73],
+  ['May', 5.86],
+  ['June', 15.46],
+  ['July', 17.18],
+  ['August', 13.48],
+  ['September',	7.41],
+  ['October', 9.50],
+  ['November',	6.86],
+  ['December',	5.00]
+]);
